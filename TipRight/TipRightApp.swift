a57c4d35f5463913e878736d0dff5c4ffbea51a0
Matthew DiNovo <matt.dinovo@icloud.com>
2025-06-13 21:16:21 -0500

    Initial Commit

---
+++ TipRight/TipRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipRightApp.swift
+//  TipRight
+//
+//  Created by Matt DiNovo on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TipRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
