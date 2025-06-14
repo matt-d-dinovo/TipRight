45490295baab0bcfa5d0bcd10da29c071d8d6e68
Matthew DiNovo <matt.dinovo@icloud.com>
2025-06-13 22:22:43 -0500

    Added basic layout, and added half-sheet

--- TipRight/ContentView.swift
+++ TipRight/ContentView.swift
@@ -8,17 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tipAmount: Double = 0
+    @State private var splitAmount: Double = 1
+    @State private var showSheet: Bool = false
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Slider(value: $tipAmount, in: 0...30 , step: 1) {
+                Text(String(tipAmount) + "%").font(.title2).fontWeight(.thin)
+            } minimumValueLabel: {
+                Text("Tip: ").font(.title2).fontWeight(.thin)
+            } maximumValueLabel: {
+                Text(String(tipAmount) + "%").font(.title2).fontWeight(.thin)
+            }
+            .padding()
+            
+            Slider(value: $splitAmount, in: 1...30, step: 1)
+            
+            
+            
+            
         }
         .padding()
+        
+    VStack() {
+            Text("Round up or down to the Nearest Dollar")
+                .font(.subheadline)
+        HStack {
+              
+            Button(" Round Up") {
+            //TODO: Add functionality to round up to the nearest dollar
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button(" Round Down") {
+            //TODO: Add functionality to round down to the nearest dollar
+            }
+            .buttonStyle(.borderedProminent)
+            
+          
+            }
+        .padding()
+        
+        Button("Upgrade to Pro") {
+            showSheet = true
+        }
+        .sheet(isPresented: $showSheet) {
+            Text("Content")
+                .presentationDetents([.height(200), .medium, .large])
+                .presentationDragIndicator(.automatic)
+        }
+        .font(.title).bold()
+        }
+        
     }
+    
 }
-
 #Preview {
     ContentView()
 }
